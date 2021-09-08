5430a655fb53d5993a2b9e3ded6c157e259676ec
grupo15quiz <carlaavaleria@gmail.com>
2021-09-08 15:35:51 -0300

    subiendo proyecto info

---
+++ Quiz/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import Pregunta, ElegirRespuesta, PreguntasRespondidas, QuizUsuario
+
+from .forms import ElegirInlineFormser
+
+class ElegirRespuestaInline(admin.TabularInline):
+    model = ElegirRespuesta
+    can_delete = False
+    max_num = ElegirRespuesta.MAXIMO_RESPUESTA
+    min_num = ElegirRespuesta.MAXIMO_RESPUESTA
+    formser = ElegirInlineFormser
+
+class PreguntaAdmin(admin.ModelAdmin):
+    model = Pregunta
+    inlines = (ElegirRespuestaInline, )
+    list_display = ["texto",]
+    search_fields = ["texto", "preguntas__texto"]
+
+class PreguntasRespondidasAdmin(admin.ModelAdmin):
+    list_display = ["pregunta", "respuesta", "correcta", "puntaje_obtenido"]
+
+    class Meta:
+        model = PreguntasRespondidas
+
+admin.site.register(PreguntasRespondidas)
+admin.site.register(Pregunta, PreguntaAdmin)
+admin.site.register(ElegirRespuesta)
+admin.site.register(QuizUsuario)
